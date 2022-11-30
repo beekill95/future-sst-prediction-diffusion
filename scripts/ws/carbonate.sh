bbdd9e744f9bcc8b3cebbb815006c98c9949d0b6
Quan Nguyen <qmnguyen@iu.edu>
2022-11-30 10:03:58 -0500

    Add scripts for starting jupyterlab server in gpu nodes

---
+++ scripts/ws/carbonate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Activate the environment.
+require_cuda=${3-true}
+if [[ $require_cuda == "true" ]]; then
+    module load cudatoolkit
+fi
+module load anaconda
+conda activate sst
+
+# Then, forward port back to h2
+server=${1-h2}
+port=${2-8888}
+echo "Logging into $server to forward port $port:"
+ssh -N -f -R $port:localhost:$port $server
+
+# Finally, start jupyter notebook at the specified port.
+echo "Starting jupyter notebook at port $port"
+python --version
+jupyter-lab --no-browser --ip 0.0.0.0 --port $port --notebook-dir=.
