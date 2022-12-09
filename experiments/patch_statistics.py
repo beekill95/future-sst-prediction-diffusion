8fe33ca5ee635ad238506f25b68943c5e5896d22
Quan Nguyen <qmnguyen@iu.edu>
2022-12-09 18:51:16 -0500

    Add statistic for difference between two consecutive days

--- experiments/patch_statistics.py
+++ experiments/patch_statistics.py
@@ -38,7 +38,7 @@ import xarray as xr
 # +
 class PatchDataset:
     def __init__(self, patch: int, year: int | None = None) -> None:
-        self._patches = PatchDataset._load_patches(patch, year)
+        self._patches = self._load_patches(patch, year)
 
     def __len__(self):
         return self._patches.shape[0]
@@ -50,8 +50,7 @@ class PatchDataset:
         for i in range(len(self)):
             yield self[i]
 
-    @staticmethod
-    def _load_patches(path: int, year: int | None) -> np.ndarray:
+    def _load_patches(self, path: int, year: int | None) -> np.ndarray:
         files = PatchDataset._list_files(path, year)
 
         patches = []
@@ -172,5 +171,102 @@ for row, patch_idx in enumerate(patches):
     ax.set_title(f'Patch {patch_idx}')
     ax.set_xlabel('Days')
     ax.set_ylabel('Temperature (degC)')
+
+fig.tight_layout()
+# -
+
+# ## Temperature Difference Variation
+
+# +
+class PatchDifferenceDataset(PatchDataset):
+    def __init__(self, patch: int, year: int | None = None) -> None:
+        super().__init__(patch, year)
+
+    def _load_patches(self, path: int, year: int | None) -> np.ndarray:
+        patches = super()._load_patches(path, year)
+        diff = patches[1:] - patches[:-1]
+        return diff
+
+
+ds = PatchDataset(1, 2006)
+diff_ds = PatchDifferenceDataset(1, 2006)
+print(len(ds), len(diff_ds))
+# -
+
+# ### Difference Statistics
+
+# +
+patches = list(range(1, 31))
+years = list(range(2006, 2018))
+
+fig, axes = plt.subplots(nrows=6, ncols=5, figsize=(16, 16), sharex=True, sharey=True)
+for patch_idx, ax in zip(patches, axes.flatten()):
+    min_temps = []
+    max_temps = []
+    mean_temps = []
+
+    for year in years:
+        ds = PatchDifferenceDataset(patch_idx, year)
+        min_temps.append(min_temp(ds))
+        max_temps.append(max_temp(ds))
+        mean_temps.append(mean_temp(ds))
+
+    # Plot the results.
+    ax.set_title(f'Patch difference {patch_idx} statistics')
+    ax.plot(years, min_temps, label='Min difference')
+    ax.plot(years, max_temps, label='Max difference')
+    ax.plot(years, mean_temps, label='Mean difference')
+    ax.set_xlabel('Years')
+    ax.set_ylabel('Temperature Difference (degC)')
+
+fig.tight_layout()
+# -
+
+# ### Temperature Difference Variation within a year
+
+# +
+patches = [1, 6, 10, 14, 16, 17, 24, 25, 28]
+years = [2006, 2010, 2015, 2017]
+fig, axes = plt.subplots(ncols=len(years), nrows=len(patches), sharey=True, figsize=(16, 16))
+for row, patch_idx in enumerate(patches):
+    for col, year in enumerate(years):
+        ax = axes[row, col]
+
+        ds = PatchDifferenceDataset(patch_idx, year)
+        min_temps = [p.min() for p in ds]
+        max_temps = [p.max() for p in ds]
+        mean_temps = [p.mean() for p in ds]
+
+        days = list(range(len(ds)))
+        ax.plot(days, min_temps)
+        ax.plot(days, max_temps)
+        ax.plot(days, mean_temps)
+        ax.set_title(f'Patch {patch_idx} in {year}')
+        ax.set_xlabel('Days')
+        ax.set_ylabel('Temperature\ndifference (degC)')
+
+fig.tight_layout()
+# -
+
+# ### Temperature Difference Variation in all years
+
+# +
+patches = [1, 6, 10, 14, 16, 17, 24, 25, 28]
+fig, axes = plt.subplots(nrows=len(patches), sharey=True, figsize=(16, 4 * len(patches)))
+for row, patch_idx in enumerate(patches):
+    ax = axes[row]
+
+    ds = PatchDifferenceDataset(patch_idx)
+    min_temps = [p.min() for p in ds]
+    max_temps = [p.max() for p in ds]
+    mean_temps = [p.mean() for p in ds]
+
+    days = list(range(len(ds)))
+    ax.plot(days, min_temps)
+    ax.plot(days, max_temps)
+    ax.plot(days, mean_temps)
+    ax.set_title(f'Patch {patch_idx}')
+    ax.set_xlabel('Days')
+    ax.set_ylabel('Temperature\nDifference (degC)')
 
 fig.tight_layout()
